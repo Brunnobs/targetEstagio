a28a9845e6add17db5071df7eb2043a407d77b05
brunosouza <brunobbbsss@gmail.com>
2024-09-16 19:19:05 -0300

    remoção da pasta .idea e incluir no gitignore

--- src/Resultado3_Soma.java
+++ src/Resultado3_Soma.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Resultado3_Soma {
+public class Resultado3_Soma {
+    public static void main(String[] args) {
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k<indice){
+            k = k + 1;
+            soma = soma + k;
+        }
+    System.out.println(soma);
+    }
 }
